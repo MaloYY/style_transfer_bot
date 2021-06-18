465d19419145e077d4fb6e7d23d48a945fec5418
Pavel Malov <kiba.malov@gmail.com>
2021-06-18 14:53:25 +0300

    add req

--- main.py
+++ main.py
@@ -27,20 +27,10 @@ dp = Dispatcher(bot)
 dp.middleware.setup(LoggingMiddleware())
 
 
-@dp.message_handler(commands=['start'])
-async def echo(message: types.Message):
-    # Regular request
-    # await bot.send_message(message.chat.id, message.text)
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
-
-    # or reply INTO webhook
-    return SendMessage(message.chat.id, message.text)
-
-
-async def on_startup(dp):
-    await bot.set_webhook(WEBHOOK_URL)
-
-    # insert code here to run it after start
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """Отправляет приветственное сообщение и помощь по боту"""
+    await message.answer('Hello ;)')
 
 
 async def on_shutdown(dp):
@@ -62,7 +52,7 @@ if __name__ == '__main__':
     start_webhook(
         dispatcher=dp,
         webhook_path=WEBHOOK_PATH,
-        on_startup=on_startup,
+        #on_startup=on_startup,
         on_shutdown=on_shutdown,
         skip_updates=True,
         host=WEBAPP_HOST,
